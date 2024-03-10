0a42c40a01cf8e643506762867a0c9c0769a48db
AissaRouk <aissa.rouk@gmail.com>
2024-03-10 17:23:34 +0200

    generic styles

---
+++ src/Styles/GenericStyles.js
@@ -0,0 +1,9 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+export default genericStyles = StyleSheet.create({
+  //the two most used colors
+});
+
+export const greyColor = "#A4AAB7",
+  blackTheme = "#171C26",
+  brightGrey = "#A4AAB7";
